c9920e5b0ede221f15539cdc395d5b4c1c37ad53
Luis F. Vasquez Solis Goycochea <61159051+Cathriel@users.noreply.github.com>
2022-05-27 16:13:13 -0500

    remove profile service

--- profile-service/src/main/java/pe/edu/upc/services/FollowService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.services;
-
-import pe.edu.upc.entities.Follow;
-
-public interface FollowService extends CrudService<Follow,Long> {
-
-    Follow createFollow(Follow follow, Long chefId, Long readerId);
-}
