a86ede08bda740d323dabff3fd3a50bd0f0256bc
Luis F. Vasquez Solis Goycochea <61159051+Cathriel@users.noreply.github.com>
2022-05-27 16:33:19 -0500

    add profile services v2

---
+++ profile-service/src/main/java/pe/edu/upc/ProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileServiceApplication.class, args);
+	}
+
+}
