5c5a76eccba28ba1be6d9b0cb14b4e7d16ae4537
lhm2001 <lhm2001@hotmail.com>
2022-05-27 16:00:01 -0500

    Fix profile-service

--- profile-service/src/main/java/pe/edu/upc/ProfileServiceApplication.java
+++ profile-service/src/main/java/pe/edu/upc/ProfileServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Proyecto01Application {
+public class ProfileServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Proyecto01Application.class, args);
+        SpringApplication.run(ProfileServiceApplication.class, args);
     }
 
 }
