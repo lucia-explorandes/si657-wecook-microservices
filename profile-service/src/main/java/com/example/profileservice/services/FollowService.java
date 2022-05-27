37cac5f5a7000dcfee511bfba822db9a0c3df42d
Luis F. Vasquez Solis Goycochea <61159051+Cathriel@users.noreply.github.com>
2022-05-27 16:32:55 -0500

    commit

--- profile-service/src/main/java/com/example/profileservice/services/FollowService.java
+++
@@ -1,8 +0,0 @@
-package com.example.profileservice.services;
-
-import pe.edu.upc.entities.Follow;
-
-public interface FollowService extends CrudService<Follow,Long> {
-
-    Follow createFollow(Follow follow, Long chefId, Long readerId);
-}
